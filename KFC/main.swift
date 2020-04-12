62ecb907721c82f5d221a02887e4120f7b25dff7
kittinun chobthum <chobtham.kittinun@gmail.com>
2020-04-12 13:39:38 +0700

    Initial Commit

---
+++ KFC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KFC
+//
+//  Created by Kittinun Chobtham on 12/4/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
